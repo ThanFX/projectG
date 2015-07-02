63df805bca7b97a40b02ff29a05318fbf6163089
ThanFX <ZMikhael@gmail.com>
2015-07-02 18:53:36 +0600

    ToDo проекта

--- scripts/mongo.js
+++ scripts/mongo.js
@@ -3,34 +3,34 @@ var Person = require('../models/person').Person;
 var Chars = require('../models/person.characteristic').PersonCh;
 var log = require('../libs/log')(module);
 
-var persons = [];
+//var persons = [];
 
 var rand = function (min, max) {
     return Math.floor(min + Math.random()*(max +1 - min));
 };
 
-var jobs = {
-    "1": {
-        jobName: "Лесоруб",
-        skillName: "Лесорубство"
-    },
-    "2": {
-        jobName: "Углежог",
-        skillName: "Углежогство"
-    },
-    "3": {
-        jobName: "Рыболов",
-        skillName: "Рыболовство"
-    },
-    "4": {
-        jobName: "Охотник",
-        skillName: "Охота"
-    },
-    "5": {
-        jobName: "Фермер",
-        skillName: "Фермерство"
-    }
-};
+// var jobs = {
+//     "1": {
+//         jobName: "Лесоруб",
+//         skillName: "Лесорубство"
+//     },
+//     "2": {
+//         jobName: "Углежог",
+//         skillName: "Углежогство"
+//     },
+//     "3": {
+//         jobName: "Рыболов",
+//         skillName: "Рыболовство"
+//     },
+//     "4": {
+//         jobName: "Охотник",
+//         skillName: "Охота"
+//     },
+//     "5": {
+//         jobName: "Фермер",
+//         skillName: "Фермерство"
+//     }
+// };
 
 //for(var i = 0; i < 99; i++){
 //    var j = rand(1,5);
@@ -46,49 +46,49 @@ var jobs = {
 //    persons.push(p);
 //}
 
-log.info("Сохраняем характеристики");
-Person.find({"job" : {$ne: "Староста"}}, function(err, persons){
-    if(err){
-        log.err(err);
-    }
-    if(!persons){
-        log.info("Пользователи не найдены");
-    } else {
-        async.each(persons, function(person){
-            var skills = [
-                {
-                    name: "Здоровье",
-                    value: 100
-                },
-                {
-                    name: "Сила",
-                    value: rand(60, 100)
-                },
-                {
-                    name: "Выносливость",
-                    value: rand(60, 100)
-                },
-                {
-                    name: "Усталость",
-                    value: 0
-                },
-                {
-                    name: "Голод",
-                    value: 0
-                },
-                {
-                    name: "Жажда",
-                    value: 0
-                }
-            ];
-            Chars.upsertPCh(person._id, "Сон", skills, {x:0,y:0}, function(err){
-                if(err) log.err(err);
-            });
-        }, function(err){
-            if(err) log.err(err);
-        });
-    }
-});
+// log.info("Сохраняем характеристики");
+// Person.find({"job" : {$ne: "Староста"}}, function(err, persons){
+//     if(err){
+//         log.err(err);
+//     }
+//     if(!persons){
+//         log.info("Пользователи не найдены");
+//     } else {
+//         async.each(persons, function(person){
+//             var skills = [
+//                 {
+//                     name: "Здоровье",
+//                     value: 100
+//                 },
+//                 {
+//                     name: "Сила",
+//                     value: rand(60, 100)
+//                 },
+//                 {
+//                     name: "Выносливость",
+//                     value: rand(60, 100)
+//                 },
+//                 {
+//                     name: "Усталость",
+//                     value: 0
+//                 },
+//                 {
+//                     name: "Голод",
+//                     value: 0
+//                 },
+//                 {
+//                     name: "Жажда",
+//                     value: 0
+//                 }
+//             ];
+//             Chars.upsertPCh(person._id, "Сон", skills, {x:0,y:0}, function(err){
+//                 if(err) log.err(err);
+//             });
+//         }, function(err){
+//             if(err) log.err(err);
+//         });
+//     }
+// });
 
 //log.info("Начинаем сохранение!");
 //async.each(persons, function(person){
