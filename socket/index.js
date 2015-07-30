4cf6c57ddc9b3e0b9f69b1020a355272510b4dfc
ThanFX <ZMikhael@gmail.com>
2015-07-30 16:31:38 +0600

    Каждые 5 секунд отправляем время мира всем подключенным клиентам по websocket

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,27 @@
 var log = require('../libs/log')(module);
 var config = require('../config');
+var timeSettings = require('../models/settings').timeSettings;
+
 
 module.exports = function(server){
+    
+	var worldDate = function(){
+	    timeSettings.getWorldTime(function(err, worldTime){
+	        if(err){
+	            log.err(err);
+	        }
+	        io.sockets.emit('worldDate', worldTime);
+	    });
+	};
+
     var io = require('socket.io').listen(server);
     io.set('origins', config.get('socket.io:origins'));
 
     io.on('connection', function(socket){
-        socket.emit('data', 'Test!');
+        var emitWorldDate = function(){
+        	worldDate();
+        	setTimeout(emitWorldDate, 5000);
+    	}
+    	emitWorldDate();
     });
 };
